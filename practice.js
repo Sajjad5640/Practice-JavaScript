2f6bbed699884bafbdfe58bc6fa94a29d3ecc284
Sajjad5640 <anik15-5640@diu.edu.bd>
2025-05-31 07:49:19 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,16 @@
+// function myfunction(){
+//            document.getElementById("demo").innerHTML = "Hello World";
+//          }
+
+// const cars = ["salam","Borkat","seli"];
+// cars.push("selma");
+// console.log(cars);
+// JavaScript Fuction
+function myFunction(name, time){
+    console.log(name + " is sleeping from " + time +" pm");
+
+}
+myFunction("kalam", "10");
+myFunction("Ralam", "3");
+myFunction("baalam", "5");
+console.log("my name is jasim");
